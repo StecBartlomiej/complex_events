a44c2dc1d5648568ed7470a8e45ce9ba5cf624d4
Roman Nowak <roman.nowak9@interia.pl>
2025-12-22 18:04:56 +0100

    bins from pos and neg events

--- src/test/precomuted_bins_view.py
+++ src/test/precomuted_bins_view.py
@@ -2,9 +2,12 @@ import numpy as np
 import cv2
 from pathlib import Path
 
-output_root = Path("./data/cifar10dvs_exp_frames")  # folder z zapisanymi .npz
+# output_root = Path("./data/cifar10dvs_raw_bins")  # folder z zapisanymi .npz
+# output_root = Path("./data/cifar10dvs_pos_raw_bins")  # folder z zapisanymi .npz
+output_root = Path("./data/cifar10dvs_all_raw_bins")  # folder z zapisanymi .npz
+# output_root = Path("./data/cifar10dvs_exp_frames")  # folder z zapisanymi .npz
 label_to_show = "0"  # klasa do wyświetlenia
-delay_ms = 300       # opóźnienie między binami w ms
+delay_ms = 200       # opóźnienie między binami w ms
 
 label_folder = output_root / label_to_show
 npz_files = sorted(label_folder.glob("*.npz"))
@@ -17,18 +20,23 @@ for npz_file in npz_files:
     with np.load(npz_file) as f:
         frame = f["data"].astype(np.uint8)
 
-    # skalowanie do 0-255 jeśli trzeba
-    if frame.max() <= 1.0:
-        frame = (frame * 255).astype(np.uint8)
+    if frame.ndim == 2:
+        frame = np.expand_dims(frame, axis=0)
 
-    # Normalizacja
-    # frame = (frame - frame.min()) / (frame.max() - frame.min()) * 255
+    for p_i in range(frame.shape[0]):
+        # Normalizacja
+        frame[p_i] = (frame[p_i] - frame[p_i].min()) / (frame[p_i].max() - frame[p_i].min()) * 255
 
-    # wyświetlenie w oknie
-    cv2.imshow(f"Class {label_to_show}", frame)
-    
+    cv2.imshow(f"Class {label_to_show} - pos", frame[0])
+    if frame.shape[0] > 1:
+        cv2.imshow(f"Class {label_to_show} - neg", frame[1])
+
+        color = np.zeros((frame[0].shape[0], frame[0].shape[1], 3), dtype=np.uint8)
+        color[:,:,0] = frame[0]
+        color[:,:,2] = frame[1]
+        cv2.imshow(f"Class {label_to_show} - all", color)
+
     key = cv2.waitKey(delay_ms)
-    # jeśli naciśnięto 'q', wyjdź
     if key & 0xFF == ord('q'):
         break
 
